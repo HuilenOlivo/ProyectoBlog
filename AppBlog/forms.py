15a871f0ca26f4f21f74ea59f96606096fda6c4a
HuilenOlivo <huilendiana@gmail.com>
2023-01-07 01:04:41 -0300

    corrigiendo cositas

--- AppBlog/forms.py
+++ AppBlog/forms.py
@@ -3,16 +3,16 @@ from django import forms
 
 
 class AutorForm(forms.Form):
-    nombre = forms.CharField (max_length = 80, blank = False, null = False, label='Nombre Autor' )
-    apellido = forms.CharField (max_length = 80, blank = False, null = False, label='Apellido' )
-    edad= forms.IntegerField (null = True, blank = True, label='Edad')
-    correo = forms.EmailField (unique = True, max_length= 100, label='Correo')
-    ubicacion = forms.CharField (max_length = 200, blank = False, null = False, label='Ubicacion geografica')
-    descripcion = forms.TextField (blank = False, null = False, label='Descripcion')
+    nombre = forms.CharField (max_length = 80, label='Nombre Autor' )
+    apellido = forms.CharField (max_length = 80, label='Apellido' )
+    edad= forms.IntegerField (label='Edad')
+    correo = forms.EmailField (max_length= 100, label='Correo')
+    ubicacion = forms.CharField (max_length = 200, label='Ubicacion geografica')
+    descripcion = forms.CharField (label='Descripcion')
 
 class ArticuloForm (forms.Form):
-    titulo = forms.CharField (max_length = 200, blank = False, null = False, label='Nombre Articulo')
-    subtitulo = forms.CharField (max_length = 200, blank = False, null = False, label='Subtitulo del Articulo')
-    cuerpo = forms.TextField (blank = False, null = False, verbose_name = 'Contenido del Articulo')
-    fecha_publicacion = forms.DateField ('Fecha de publicacion', blank = False, null = False, label='Fecha de Publicacion')
+    titulo = forms.CharField (max_length = 200, label='Nombre Articulo')
+    subtitulo = forms.CharField (max_length = 200, label='Subtitulo del Articulo')
+    cuerpo = forms.CharField (label = 'Contenido del Articulo')
+    fecha_publicacion = forms.DateField (label='Fecha de Publicacion')
     
--- AppBlog/views.py
+++ AppBlog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import *
+from .forms import *
+
 
 # Create your views here.
 def Inicio (request):
