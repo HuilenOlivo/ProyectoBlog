8d782b77c487fa59562f99fa77036f9fbcec73fe
HuilenOlivo <huilendiana@gmail.com>
2023-01-11 18:08:10 -0300

    Sumando formulario

--- AppBlog/urls.py
+++ AppBlog/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
 
     path ('', Inicio, name = 'inicio'),
     path ('autores/', autores, name = 'autores'),
-    path ('crear_autor/', crearAutor, name = 'crear_autor'),
+    path ('crear_autor/', crearautor, name = 'crear_autor'),
     path ('articulos/', articulos, name='articulos'),
     path ('crear_articulo/', crear_articulo, name= 'crear_articulo'),    
    
--- AppBlog/views.py
+++ AppBlog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import *
-from .forms import *
+from AppBlog.forms import *
 
 
 # Create your views here.
@@ -14,10 +14,22 @@ def articulos (request):
     return render (request, 'AppBlog/articulos.html')
 
 def crear_articulo (request):
-    return render (request, 'AppBlog/crear_articulo.html')
+    if request.method == 'POST':
+        tituloarticulo= request.POST ['titulo_articulo']
+        subtituloarticulo= request.POST ['subtitulo_articulo']
+        cuerpoarticulo= request.POST ['cuerpo_articulo']
+        autorarticulo= request.POST ['autor_id']
+        publicacionarticulo= request.POST ['publicacion_articulo']
+        articulocreado= Articulo(tituloarticulo=tituloarticulo, subtituloarticulo=subtituloarticulo, cuerpoarticulo=cuerpoarticulo, autorarticulo=autorarticulo, publicacionarticulo=publicacionarticulo)
+        articulocreado.save()
+        return render(request, "AppBlog/articulos.html", {'mensaje': 'Articulo Guardado'})
+    
+
+    else:
+        return render (request, 'AppBlog/crear_articulo.html')
 
 
-def crearAutor (request):
+'''def crearAutor (request):
     if request.method == 'POST':
         autor_form = AutorForm(request.POST)
 
@@ -39,6 +51,28 @@ def crearAutor (request):
 
     else:
         formulario = AutorForm()
-        return render (request, 'AppBlog/crear_autor.html', {'form': formulario}) 
+        return render (request, 'AppBlog/crear_autor.html', {'form': formulario}) '''
 
 
+def crearautor (request):
+    if request.method=='POST':
+        form= AutorForm (request.POST)
+        if form.is_valid():
+            informacion=form.cleaned_data
+            nombre= informacion['nombre']
+            apellido= informacion ['apellido']
+            edad= informacion ['edad']
+            correo=informacion['correo']
+            ubicacion=informacion['ubicacion']
+            descripcion= informacion ['descripcion']
+            autorcito= Autor (nombre=nombre, apellido=apellido, edad=edad, correo=correo, ubicacion=ubicacion, descripcion=descripcion)
+            autorcito.save()
+            autores= Autor.objects.all()
+            return render (request, 'AppBlog/autores.html', {"autores": autores, "mensaje": "Autor Creado Correctamente"})
+        
+        else:
+            return render(request,'AppBlog/crear_autor.html', {"form": form, "mensaje": "Informacion no valida"} )
+
+    else:
+        formulario= AutorForm
+        return render (request, 'AppBlog/crear_autor.html', {'form': formulario})
